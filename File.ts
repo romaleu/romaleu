bd9418a14b2a69b95b3f5fff51842d006aa118f0
romaleu <159131506+romaleu@users.noreply.github.com>
2024-04-17 18:16:51 -0300

    Update and rename Teste.MD to File.ts

---
+++ File.ts
@@ -0,0 +1,2 @@
+[![Fuzzing](https://github.com/nodejs/undici/actions/workflows/fuzz.yml/badge.svg)](https://github.com/nodejs/undici/actions/workflows/fuzz.yml)
+[![CI](https://github.com/typescript-eslint/typescript-eslint/actions/workflows/ci.yml/badge.svg)](https://github.com/typescript-eslint/typescript-eslint/actions/workflows/ci.yml)
